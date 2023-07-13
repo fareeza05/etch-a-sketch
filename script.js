91d9435f9de94539b0f7eecf656e18fbd5ee8582
fareeza05 <fareezarahman123@gmail.com>
2023-07-13 19:30:38 -0400

    added screen to display mode

--- script.js
+++ script.js
@@ -62,13 +62,14 @@ document.querySelector('body').addEventListener('click', (e)=>{
     if(e.target.tagName != "BUTTON"){
         click =!click;
         if (click) {
-            document.querySelector('.mode').textContent = 'Mode: Coloring.'
+            document.querySelector('.mode').textContent = 'Mode: Coloring (≧◡≦) '
         } else {
-            document.querySelector('.mode').textContent = 'Mode: Not Coloring.'
+            document.querySelector('.mode').textContent = 'Mode: Not Coloring (」°ロ°)」'
+        
         }
     }
 });
 
-// add select color drop down menu & connect it to the JS
+
 // add "mode" display
 // add functionality to "darken button" -> gradual change in opacity
